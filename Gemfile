cb1c4328901d3fb828ea4e3228653568a329a09d
apo5698 <dingdong.exe@gmail.com>
2020-06-10 20:06:04 -0400

    fix: resolve library dependencies issue

--- Gemfile
+++ Gemfile
@@ -53,9 +53,6 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
-# Bootswatch for Simplex theme
-gem 'bootswatch'
-
 # Use session to store cookie
 gem 'activerecord-session_store', github: 'rails/activerecord-session_store'
 
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,8 +5,8 @@ module.exports = environment
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
     new webpack.ProvidePlugin({
-        $: 'jquery/src/jquery',
-        jQuery: 'jquery/src/jquery',
-        Popper: ['popper.js', 'default']
+        // $: 'jquery',
+        // jQuery: 'jquery',
+        // Popper: ['popper.js', 'default']
     })
 )
