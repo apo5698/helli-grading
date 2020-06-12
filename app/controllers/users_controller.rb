6280e3fb75bd0e17bdc0b1586b571c5e4e2f5a41
uiz-11 <ui.zhang11@gmail.com>
2020-06-11 21:19:18 -0400

    refactor: change form_tag to form_with

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
+    params[:user][:phone_number] = params[:user][:phone_number].tr('-', '') if params[:user][:phone_number]
     params.require(:user).permit(:first_name, :last_name, :email,
                                  :phone_number, :date_of_birth,
                                  :gender, :path_to_avatar)
