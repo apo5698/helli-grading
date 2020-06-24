115e46fe7222802dcd6bf859a6edb900cce5e685
Yulin Zhang <yzhan114@ncsu.edu>
2020-06-24 19:10:40 -0400

    ?????

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_221331) do
+ActiveRecord::Schema.define(version: 2020_06_24_230921) do
 
   create_table "assignments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "type"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 2020_06_24_221331) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "uuid"
   end
 
 end
