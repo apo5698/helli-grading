02286b89b984a3ba2efd26cc442c24dfc78064e1
apo5698 <dingdong.exe@gmail.com>
2020-06-27 02:23:30 -0400

    java compilation and execution support

---
+++ app/helpers/java_helper.rb
@@ -0,0 +1,39 @@
+require 'open3'
+
+module JavaHelper
+  # Execute a command with arguments. Returns a hash containing stdout, stderr,
+  # and status.
+  def self.exec(cmd, *args)
+    puts args
+    args = args.map(&:to_s).reject(&:empty?).map { |e| "'#{e}'" }.join(' ')
+    stdout, stderr, status = Open3.capture3(cmd + ' ' + args)
+    puts stdout
+    puts stderr
+    { stdout: stdout, stderr: stderr, status: status }
+  end
+
+  @junit_bundle = "#{ConfigHelper.lib_path('junit4')}/*:#{ConfigHelper.lib_path('hamcrest-core')}/*"
+
+  # Runs a compiled Java file (*.class).
+  def self.java(file:, junit: false, opt: [])
+    cp = File.dirname(file)
+    cp += ":#{@junit_bundle}" if junit
+    junit_pkg = junit ? 'org.junit.runner.JUnitCore' : ''
+    classname = File.basename(file).sub('.class', '')
+    exec('java', '-cp', cp, junit_pkg, classname, opt.join(' '))
+  end
+
+  # Compiles a Java file (*.java).
+  def self.javac(file:, junit: false, opt: [])
+    dirname = File.dirname(file)
+    cp = dirname
+    cp += ":#{@junit_bundle}" if junit
+    exec('javac', '-d', dirname, '-cp', cp, file, opt.join(' '))
+  end
+
+  # Runs checkstyle on a Java file (*.java).
+  def self.checkstyle(file)
+    path = ConfigHelper.path('checkstyle')
+    exec(path, file)
+  end
+end
