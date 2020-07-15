7637b34118ca7746f4a57705848bf9b4b2ebf5ee
Yulin Zhang <yzhan114@ncsu.edu>
2020-07-15 10:46:25 -0400

    ts_file destroy_selected

--- app/controllers/ts_files_controller.rb
+++ app/controllers/ts_files_controller.rb
@@ -1,8 +1,8 @@
 class TsFilesController < ApplicationController
+  before_action :set_variables
+
   def index
-    ts_file = TsFile.find_by(assignment_id: @assignment.id)
-    ts_file ||= TsFile.new
-    @ts_files = ts_file.files
+    @ts_files = @ts_file.files
   end
 
   def upload
@@ -18,5 +18,25 @@ class TsFilesController < ApplicationController
     redirect_back(fallback_location: '')
   end
 
+  def destroy_selected
+    selected = params[:files]&.select { |_, v| v.to_i == 1 }
+    if selected.nil?
+      flash[:error] = 'No file found.'
+    elsif selected.empty?
+      flash[:error] = 'No file selected.'
+    else
+      selected.each do |selected_id, _|
+        @ts_file.files.find(selected_id).purge
+      end
+      flash[:success] = 'Selected file(s) deleted.'
+    end
+    redirect_back(fallback_location: '')
+  end
+
   private
+
+  def set_variables
+    super
+    @ts_file = TsFilesHelper.create(@assignment.id)
+  end
 end
