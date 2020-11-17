850f6920aed957d093d2d1e4ba148c755732bd87
apo5698 <dingdong.exe@gmail.com>
2020-11-16 23:08:13 -0500

    feat: add to_b methods

--- config/initializers/rails_override.rb
+++
@@ -1,9 +0,0 @@
-ActiveStorage::Attached::Many.class_eval do
-  def find_by_filename(name)
-    attachments.find { |f| f.filename == name }
-  end
-
-  def delete_by_filename(name)
-    find_by_filename(name).purge
-  end
-end
