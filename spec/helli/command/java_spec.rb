6fc876cc01a14d80caf4d721236976843c86d5eb
apo5698 <dingdong.exe@gmail.com>
2020-11-21 21:57:12 -0500

    test(command_java): test error handling

--- spec/helli/command/java_spec.rb
+++ spec/helli/command/java_spec.rb
@@ -3,43 +3,81 @@ describe Helli::Command::Java do
   before(:all) { Dir.glob("#{fixtures}/**/*.class").each { |f| File.delete(f) } }
 
   describe '.javac' do
-    context 'when file is valid' do
+    context 'when file has no error' do
       Dir.glob("#{fixtures}/wce/compile/valid/*.java").each do |file|
         process = described_class.javac(file)
-        it('javac successfully') { expect(process.exitstatus).to be_zero }
+        it('returns zero') { expect(process.exitstatus).to be_zero }
         it('stdout is empty') { expect(process.stdout).to be_blank }
         it('stderr is empty') { expect(process.stderr).to be_blank }
       end
     end
 
-    context 'when file is invalid' do
+    context 'when file has errors' do
       Dir.glob("#{fixtures}/wce/compile/invalid/*.java").each do |file|
         process = described_class.javac(file)
-        it('javac failed') { expect(process.exitstatus).not_to be_zero }
+        it('returns non-zero') { expect(process.exitstatus).not_to be_zero }
         it('stdout is empty') { expect(process.stdout).to be_blank }
         it('stderr is not empty') { expect(process.stderr).not_to be_blank }
+      end
+    end
+
+    context 'when file is not a java file' do
+      it 'raises Helli::UnsupportedFileTypeError' do
+        expect { described_class.javac("#{fixtures}/misc/NotJava.txt") }.to raise_error(Helli::UnsupportedFileTypeError)
+      end
+    end
+
+    context 'when file does not exist' do
+      it 'raises Helli::FileNotFoundError' do
+        filename = 'What.java'
+        raise "remove /#{filename} to run this test" if File.exist?(filename)
+
+        expect { described_class.javac(filename) }.to raise_error(Helli::FileNotFoundError)
       end
     end
   end
 
   describe '.java' do
-    context 'when file is valid' do
+    context 'when file has no error' do
       Dir.glob("#{fixtures}/wce/execute/valid/*.java").each do |file|
         described_class.javac(file)
         process = described_class.java(file)
-        it('java successfully') { expect(process.exitstatus).to be_zero }
+        it('returns zero') { expect(process.exitstatus).to be_zero }
         it('stdout is not empty') { expect(process.stdout).not_to be_blank }
         it('stderr is empty') { expect(process.stderr).to be_blank }
       end
     end
 
-    context 'when file is invalid' do
+    context 'when file has errors' do
       Dir.glob("#{fixtures}/wce/execute/invalid/*.java").each do |file|
         described_class.javac(file)
         process = described_class.java(file)
-        it('java failed') { expect(process.exitstatus).not_to be_zero }
+        it('returns non-zero') { expect(process.exitstatus).not_to be_zero }
         it('stdout is empty') { expect(process.stdout).to be_blank }
         it('stdout is not empty') { expect(process.stderr).not_to be_blank }
+      end
+    end
+
+    context 'when file has not been compiled' do
+      Dir.glob("#{fixtures}/wce/execute/valid/*.java").each do |file|
+        it 'raises Helli::FileNotFoundError' do
+          expect { described_class.java(file) }.to raise_error(Helli::Command::Java::ClassFileNotFoundError)
+        end
+      end
+    end
+
+    context 'when file is not a java file' do
+      it 'raises Helli::UnsupportedFileTypeError' do
+        expect { described_class.java("#{fixtures}/misc/NotJava.txt") }.to raise_error(Helli::UnsupportedFileTypeError)
+      end
+    end
+
+    context 'when file does not exist' do
+      it 'raises Helli::FileNotFoundError' do
+        filename = 'What.java'
+        raise "remove /#{filename} to run this test" if File.exist?(filename)
+
+        expect { described_class.java(filename) }.to raise_error(Helli::FileNotFoundError)
       end
     end
   end
