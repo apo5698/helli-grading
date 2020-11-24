c015ba53ab83b448de0a7d2b074f9bb4a718095d
apo5698 <dingdong.exe@gmail.com>
2020-11-23 23:49:27 -0500

    fix(initializer): change dependencies autoloading
    Heroku server does not have Rails::Server defined

--- config/initializers/dependencies.rb
+++ config/initializers/dependencies.rb
@@ -1,5 +1,12 @@
-# Only loads dependencies when running server, not during tasks
-if defined?(::Rails::Server)
-  Helli::Dependency.load('config/dependencies.yml')
-  Helli::Dependency.download_all
+begin
+  ActiveRecord::Base.connection
+rescue ActiveRecord::NoDatabaseError
+  return
+else
+  Rails.configuration.to_prepare do
+    if Helli::Dependency.table_exists?
+      Helli::Dependency.load(ENV['DEPENDENCIES_FILE'])
+      Helli::Dependency.download_all
+    end
+  end
 end
