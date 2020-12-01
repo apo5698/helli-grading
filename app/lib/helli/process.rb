fdb3c408d0ab74ae5ab4c37a164c5609fc86d220
apo5698 <dingdong.exe@gmail.com>
2020-11-30 21:23:34 -0500

    refactor(process): wd can be nil now, indicating no chdir needed

--- app/lib/helli/process.rb
+++ app/lib/helli/process.rb
@@ -9,7 +9,7 @@ class Helli::Process
 
   # Creates a Process in at given working directory.
   def initialize(wd = nil)
-    @wd = wd || Rails.root
+    @wd = wd
   end
 
   # Opens a process and captures the standard output and the standard error of a command.
@@ -18,24 +18,33 @@ class Helli::Process
     @cmd = cmd.join(' ')
     @stdin = stdin
 
-    Dir.chdir(@wd) do
-      Open3.popen3(@cmd) do |i, o, e, t|
-        i.puts @stdin
-        i.close
-
-        begin
-          Timeout.timeout(timeout) do
-            @stdout = o.read
-            @stderr = e.read
-            @exitstatus = t.value.exitstatus
-          end
-        rescue Timeout::Error
-          Process.kill('KILL', t.pid)
-          raise Timeout::Error, 'process timeout exceeded'
-        end
+    # Avoid unnecessary chdir
+    if @wd
+      Dir.chdir(@wd) do
+        _popen3(timeout)
       end
+    else
+      _popen3(timeout)
     end
 
     self
+  end
+
+  private def _popen3(timeout: 5)
+    Open3.popen3(@cmd) do |i, o, e, t|
+      i.puts @stdin
+      i.close
+
+      begin
+        Timeout.timeout(timeout) do
+          @stdout = o.read
+          @stderr = e.read
+          @exitstatus = t.value.exitstatus
+        end
+      rescue Timeout::Error
+        Process.kill('KILL', t.pid)
+        raise Timeout::Error, 'process timeout exceeded'
+      end
+    end
   end
 end
