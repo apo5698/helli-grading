efda89fae4dfe580f874ba1d72768947ac140539
Yulin Zhang <yzhan114@ncsu.edu>
2021-01-08 19:15:01 -0500

    refactor(react): react_on_rails -> react-rails

--- app/controllers/grade_items_controller.rb
+++ app/controllers/grade_items_controller.rb
@@ -1,6 +1,8 @@
 class GradeItemsController < AssignmentsViewController
   include GradingHelper
 
+  skip_before_action :verify_authenticity_token, :only => :run
+
   before_action lambda {
     id = params[:id] || params[:grade_item_id]
     @grade_item = GradeItem.find(id)
@@ -23,18 +25,10 @@ class GradeItemsController < AssignmentsViewController
   def show; end
 
   def run
-    # if @grade_item
-    #   flash[:error] = 'The rubric has not been completed. '\
-    #                   "#{helpers.link_to 'Complete',
-    #                                      course_assignment_rubric_items_path(@course, @assignment)}.".html_safe
-    #   redirect_back fallback_location: { action: :show }
-    #   return
-    # end
-
     options = params.require(:options).permit!.to_h
 
     @grade_item.run(options)
-    flash[:success] = "Run #{@rubric_item} complete."
+    flash.notice = "Run #{@rubric_item} complete."
 
     respond_to do |format|
       format.json { render json: react_grading_page_grade_item(@course, @assignment, @rubric_item, @grade_item).to_json }
