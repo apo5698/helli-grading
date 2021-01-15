2575f83f55b30dfce267c811f932852a1aed0f80
apo5698 <dingdong.exe@gmail.com>
2021-01-15 02:31:00 -0500

    refactor(rubrics::item::zybooks): remove unnecessary methods

--- app/models/rubrics/item/zybooks.rb
+++ app/models/rubrics/item/zybooks.rb
@@ -4,16 +4,6 @@ module Rubrics
   module Item
     # Rubric for calculating zyBooks grades.
     class Zybooks < Base
-      class << self
-        def default_criteria
-          []
-        end
-
-        def requirements
-          [:file]
-        end
-      end
-
       def run(_, options) end
     end
   end
