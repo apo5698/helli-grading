8ae842422fc82eb39fa73634c80461cbcb97b997
apo5698 <dingdong.exe@gmail.com>
2021-02-01 14:20:07 -0500

    refactor(api): return HTTP 404 on ActiveRecord::RecordNotFound

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -2,8 +2,12 @@
 
 module Api
   class ApplicationController < ActionController::API
-    rescue_from ActiveRecord::RecordNotFound, ActiveRecord::RecordInvalid do |exception|
+    rescue_from ActiveRecord::RecordInvalid do |exception|
       render json: exception, status: :unprocessable_entity
+    end
+
+    rescue_from ActiveRecord::RecordNotFound do |exception|
+      render json: exception, status: :not_found
     end
   end
 end
