d695d019fb4dab4f38e2032b3cf4af6bb586a36a
apo5698 <dingdong.exe@gmail.com>
2021-02-12 10:58:46 -0500

    test(jdk): fix test errors

--- spec/lib/jdk_spec.rb
+++ spec/lib/jdk_spec.rb
@@ -7,25 +7,25 @@ describe JDK do
   describe '.javac' do
     context 'when file has no error' do
       Dir.glob("#{fixtures}/wce/compile/valid/*.java").each do |file|
-        captures = described_class.javac(file)
-        it('stdout is empty') { expect(captures[0]).to be_blank }
-        it('stderr is empty') { expect(captures[1]).to be_blank }
-        it('returns zero') { expect(captures[2].exitstatus).to be_zero }
+        capture = described_class.javac(file)
+        it('stdout is empty') { expect(capture.stdout).to be_blank }
+        it('stderr is empty') { expect(capture.stderr).to be_blank }
+        it('returns zero') { expect(capture.exitstatus).to be_zero }
       end
     end
 
     context 'when file has errors' do
       Dir.glob("#{fixtures}/wce/compile/invalid/*.java").each do |file|
-        captures = described_class.javac(file)
-        it('stdout is empty') { expect(captures[0]).to be_blank }
-        it('stderr is not empty') { expect(captures[1]).not_to be_blank }
-        it('returns non-zero') { expect(captures[2].exitstatus).not_to be_zero }
+        capture = described_class.javac(file)
+        it('stdout is empty') { expect(capture.stdout).to be_blank }
+        it('stderr is not empty') { expect(capture.stderr).not_to be_blank }
+        it('returns non-zero') { expect(capture.exitstatus).not_to be_zero }
       end
     end
 
     context 'when file is not a java file' do
       it 'raises Helli::UnsupportedFileTypeError' do
-        expect(described_class.javac("#{fixtures}/misc/NotJava.txt")[2].exitstatus).not_to be_zero
+        expect(described_class.javac("#{fixtures}/misc/NotJava.txt").exitstatus).not_to be_zero
       end
     end
 
@@ -44,25 +44,25 @@ describe JDK do
       Dir.glob("#{fixtures}/wce/execute/valid/*.java").each do |file|
         described_class.javac(file)
         captures = described_class.java(file)
-        it('stdout is not empty') { expect(captures[0]).not_to be_blank }
-        it('stderr is empty') { expect(captures[1]).to be_blank }
-        it('returns zero') { expect(captures[2].exitstatus).to be_zero }
+        it('stdout is not empty') { expect(captures.stdout).not_to be_blank }
+        it('stderr is empty') { expect(captures.stderr).to be_blank }
+        it('returns zero') { expect(captures.exitstatus).to be_zero }
       end
     end
 
     context 'when file has errors' do
       Dir.glob("#{fixtures}/wce/execute/invalid/*.java").each do |file|
         described_class.javac(file)
-        captures = described_class.java(file)
-        it('stdout is empty') { expect(captures[0]).to be_blank }
-        it('stdout is not empty') { expect(captures[1]).not_to be_blank }
-        it('returns non-zero') { expect(captures[2].exitstatus).not_to be_zero }
+        capture = described_class.java(file)
+        it('stdout is empty') { expect(capture.stdout).to be_blank }
+        it('stdout is not empty') { expect(capture.stderr).not_to be_blank }
+        it('returns non-zero') { expect(capture.exitstatus).not_to be_zero }
       end
     end
 
     context 'when file is not a java file' do
       it 'raises Helli::UnsupportedFileTypeError' do
-        expect(described_class.java("#{fixtures}/misc/NotJava.txt")[2].exitstatus).not_to be_zero
+        expect(described_class.java("#{fixtures}/misc/NotJava.txt").exitstatus).not_to be_zero
       end
     end
 
@@ -79,19 +79,19 @@ describe JDK do
   describe '.checkstyle' do
     context 'when file has no warnings' do
       Dir.glob("#{fixtures}/checkstyle/valid/*.java").each do |file|
-        captures = described_class.checkstyle(file)
-        it('stdout is not empty') { expect(captures[0]).not_to be_blank }
-        it('stderr is empty') { expect(captures[1]).to be_blank }
-        it('checkstyle successfully') { expect(captures[2].exitstatus).to be_zero }
+        capture = described_class.checkstyle(file)
+        it('stdout is not empty') { expect(capture.stdout).not_to be_blank }
+        it('stderr is empty') { expect(capture.stderr).to be_blank }
+        it('checkstyle successfully') { expect(capture.exitstatus).to be_zero }
       end
     end
 
     context 'when file has warnings' do
       Dir.glob("#{fixtures}/checkstyle/invalid/*.java").each do |file|
-        captures = described_class.checkstyle(file)
-        it('stdout is not empty') { expect(captures[0]).not_to be_blank }
-        it('stderr is empty') { expect(captures[1]).to be_blank }
-        it('checkstyle successfully') { expect(captures[2].exitstatus).to be_zero }
+        capture = described_class.checkstyle(file)
+        it('stdout is not empty') { expect(capture.stdout).not_to be_blank }
+        it('stderr is empty') { expect(capture.stderr).to be_blank }
+        it('checkstyle successfully') { expect(capture.exitstatus).to be_zero }
       end
     end
   end
